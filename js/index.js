02c2fb97eb56edb534f4e897098c66f7e6c3cfdb
Pooja Pathak <33315652+pooja1pathak@users.noreply.github.com>
2019-05-29 16:54:30 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ var args = getopts(location.search,
   default:
   {
     ws_uri: 'ws://' + location.hostname + ':8888/kurento',
-    file_uri: 'file:///tmp/kurento-recording.mp4',
+    file_uri: 'file:///tmp/kurento-recording.webm',
     ice_servers: undefined
   }
 });
